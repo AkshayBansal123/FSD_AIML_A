19a133e773f62e91e0be162198af3898aef882fc
AkshayBansal123 <ab2428020@gmail.com>
2024-10-16 10:37:38 +0530

    added js files

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,38 @@
-// Combine both changes
-console.log("Current branch changes");
-console.log("Incoming changes from dev_1");
+
+const add1=(a,b)=>{
+    return a+b;
+}
+const diff=(a,b)=>a-b;
+console.log(add1(7,5));
+console.log(diff(2,4));
+var a=12;
+/*if(a>10)
+{
+    a=30;
+    var b=67;
+    console.log(a);
+    console.log(b);
+}
+console.log(a);
+console.log(b);*/
+const person={
+    name:"xyz",
+    age:20,
+}
+const newPerson={...person,name:"ABC"};
+newPerson.age=35;
+console.log(person.age);
+console.log(newPerson.age);
+console.log(person.name);
+console.log(newPerson.name);
+const emp=["xyz",30,10101.50];
+const newEmp=[...emp];
+const newEmp1=[1,...emp];
+console.log(emp[0]);
+console.log(newEmp[0]);
+console.log(newEmp1[0]);//using the spread operator
+//template literal
+const firstName="Achiever";
+const lastName="Success";
+console.log(`${firstName}  ${lastName}`);
+
