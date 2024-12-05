175b16d84c8163f53b5e213b13d8b4774388b4b9
AkshayBansal123 <ab2428020@gmail.com>
2024-12-05 15:03:04 +0530

    completed stopwatch

--- react-app1/src/components/Stopwatch.jsx
+++ react-app1/src/components/Stopwatch.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 const Stopwatch = () => {
     const [isRunning,setisRunning]=useState(false)
+    const [time,setTime]=useState(0);
+    const [isvalid,setisvalid]=useState(null)
     function handleStartStop(){
         setisRunning((prev)=>!prev)
+        clearInterval(isvalid(id))
     }
+    function handleRestart(){
+     setTime(0);
+     setisRunning(false);
+    }
+    function timeformat(time)
+    {
+      const hours=Math.floor(time/3600)
+      const rem=(time%3600);
+      const minutes=Math.floor(rem/60)
+      const seconds=rem%60;
+      return `${hours}:${minutes}:${seconds}`
+    }
+    useEffect(()=>{
+      if(isRunning)
+      {
+        const id=setInterval(()=>{
+        setTime((time)=>(time+1))
+        },1000)
+        setisvalid(id);
+      }
+      return ()=>clearInterval(isvalid)
+    },[isRunning])
   return (
     <div>
     <h1>Stopwatch app</h1>
-    <h1>Timer</h1>
+    <h1>{timeformat(time)}</h1>
     <button onClick={handleStartStop}>{isRunning? 'stop':'start'}</button>
-    <button>Reset</button>
+    <button onClick={handleRestart}>Reset</button>
     </div>
   )
 }
