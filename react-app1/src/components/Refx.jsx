50d04ac5d820ddaba6e4599bbdd763b5cf3a42a2
AkshayBansal123 <ab2428020@gmail.com>
2024-12-11 09:16:11 +0530

    modified refex

--- react-app1/src/components/Refx.jsx
+++ react-app1/src/components/Refx.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import {useRef,useState} from 'react'
 const Refx=()=>{
     const count=useRef(0);
- 
+   let a=5
     console.log("object rendered")
     function increment(){
         count.current++;
         console.log(count.current)
+        if(count.current==5)
+            alert("refcount" + count.current);
     }
     return(
         <div>
             <h1>Ref Counter: {count.current}</h1>
-          
             <button onClick={increment}>Incrementbutton</button>
+            <p>{a}</p>
         </div>
     )
 }
